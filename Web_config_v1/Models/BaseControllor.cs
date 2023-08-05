b87eeb8495aaecb446c7ea3fb05543597e944a4a
lehau <lethehau90.vn@gmail.com>
2023-08-05 19:15:16 +0700

    Update

---
+++ Web_config_v1/Models/BaseControllor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_config_v1.Models.Entity;
+
+namespace Web_config_v1.Models
+{
+    public class BaseControllor  : Controller
+    {
+        public Web_config_v1Entities connect_entity = new Web_config_v1Entities();
+    }
+}
