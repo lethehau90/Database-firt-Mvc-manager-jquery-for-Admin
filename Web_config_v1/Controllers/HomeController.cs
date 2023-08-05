1a09992b7c9b06d16188c29331b0be547f39275f
lehau <lethehau90.vn@gmail.com>
2023-08-05 22:47:29 +0700

    update

--- Web_config_v1/Controllers/HomeController.cs
+++ Web_config_v1/Controllers/HomeController.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Web_config_v1.Models;
+using Web_config_v1.Models.Entity;
 
 namespace Web_config_v1.Controllers
 {
-     [Authorize(Roles = "Admin")]
-    public class HomeController : Controller
+    // [Authorize(Roles = "Admin")]
+    public class HomeController : BaseControllor
     {
-
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
             return View();
         }
-        
-     
-       
 
-      
-      
+        public ActionResult _Icon_web()
+        {
+            var data = connect_entity.Configs.Take(1).ToList();
+            return PartialView(data);
+        }
+
+
+
+
     }
 }
