a6340702ca8c6c60aed099ba5ae8ddb8a15b3dcc
lehau <lethehau90.vn@gmail.com>
2023-08-06 07:32:38 +0700

    update view layout

--- Web_config_v1/App_Start/RouteConfig.cs
+++ Web_config_v1/App_Start/RouteConfig.cs
@@ -14,6 +14,66 @@ namespace Web_config_v1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "gioi-thieu",
+                url: "gioi-thieu",
+                defaults: new { controller = "Home", action = "introduce" }
+            );
+
+            routes.MapRoute(
+               name: "lien-he",
+               url: "lien-he",
+               defaults: new { controller = "Home", action = "contace" }
+           );
+
+            routes.MapRoute(
+                name: "trinh-chieu",
+                url: "trinh-chieu/{tagSlieshow}",
+                defaults: new { controller = "Home", action = "Slieshow", tagSlieshow = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "san-pham",
+                url: "san-pham/{Category_tag}",
+                defaults: new { controller = "Home", action = "CategoryNews", Category_tag = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "chi-tiet-san-pham",
+                url: "san-pham/{Category_tag}/{tag}",
+                defaults: new { controller = "Home", action = "CategoryNewsDetail", Category_tag = UrlParameter.Optional, tag = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "gallery",
+               url: "gallery",
+               defaults: new { controller = "Home", action = "Gallery", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "category-gallery",
+               url: "gallery/{Category_tag}",
+               defaults: new { controller = "Home", action = "CategoryGallery", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+              name: "category-gallery-detail",
+              url: "gallery/{Category_tag}/{tag}",
+              defaults: new { controller = "Home", action = "CategoryGalleryDetail", Category_tag = UrlParameter.Optional, tag = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+                name: "dich-vu",
+                url: "dich-vu",
+                defaults: new { controller = "Home", action = "News", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "chi-tiet-dich-vu",
+                url: "dich-vu/{tag}",
+                defaults: new { controller = "Home", action = "NewsDetails", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Web_config_v1/Controllers/ContactController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_config_v1.Models;
+using Web_config_v1.Models.Data;
+using Web_config_v1.Models.Service;
+
+namespace Web_config_v1.Controllers
+{
+    public class ContactController : BaseControllor
+    {
+        //
+        // GET: /Contact/
+        Contact_Service Connect = new Contact_Service();
+        public ActionResult Image_contact()
+        {
+            var data = connect_entity.Advertises.Where(x => x.PageId == 5 && x.Active == true).Take(1).ToList();
+            return PartialView(data);
+        }
+        public ActionResult DiaChi_contact()
+        {
+            var data = connect_entity.Htmls.Where(x => x.type == 8 && x.Active == 1).Take(1).ToList();
+            return PartialView(data);
+        }
+        public ActionResult OpenOffice_contact()
+        {
+            var data = connect_entity.Htmls.Where(x => x.type == 9 && x.Active == 1).Take(1).ToList();
+            return PartialView(data);
+        }
+        public ActionResult introduction_contact()
+        {
+            var data = connect_entity.Htmls.Where(x => x.type == 7 && x.Active == 1).Take(1).ToList();
+            return PartialView(data);
+        }
+
+        [HttpPost]
+        public ActionResult Save_contact(string Inputname, string InputEmail, string Inputphone, string Inputnote)
+        {
+            int Resurt;
+            try
+            {
+                Contact_Model model = new Contact_Model();
+                model.Name = Inputname;
+                model.Mail = InputEmail;
+                model.Tel = Inputphone;
+                model.Name = Inputname;
+                model.Detail = Inputnote;
+                model.Date = DateTime.Now;
+                model.Active = 0;
+
+                Connect.Create(model);
+                Resurt = 1;
+                return Json(Resurt, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Json("", JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult Map_contact()
+        {
+            var data = connect_entity.Configs.Take(1).ToList();
+            return PartialView(data);
+        }
+
+    }
+}
